5b8346b916e3472d37574abf39a9d4253738863f
AayushMohan <aayushmohan1702@gmail.com>
2021-12-18 02:34:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
 import CollectionCard from "./Components/CollectionCard";
 import Header from "./Components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x44359B0D8eB3eeF4AeFab7a452b8c9aBD276a531&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
---
+++ src/Components/Punklist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Punklist = () => {
+  return <div></div>;
+};
+
+export default Punklist;
