a87fc3b093d7ab0d2bc00a681362c090edbf7b0b
AayushMohan <aayushmohan1702@gmail.com>
2021-12-18 01:40:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./Components/CollectionCard";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/QmQAcLywRXMBs1YMumL29z9LEyPK3rJGcKKUAZjpYyKi6d"
+      />
     </div>
   );
 }
