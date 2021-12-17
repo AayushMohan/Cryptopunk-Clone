459c5fa40cf32082d072da2d74f3c2ab1d34e08c
AayushMohan <aayushmohan1702@gmail.com>
2021-12-18 00:46:00 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 const Header = () => {
   return (
@@ -21,6 +22,12 @@ const Header = () => {
         <p>Drops</p>
         <p>Marketplace</p>
         <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} alt="" />
+        </div>
       </div>
     </div>
   );
